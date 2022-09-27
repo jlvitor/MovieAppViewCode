5dadda6724c03b13b084322589152b22597b1921
Jean Lucas Vitor <jeanlucasvitor@gmail.com>
2022-09-27 18:52:39 -0300

    Desafio MovieApp concluido

---
+++ Pods/Target Support Files/Pods-ViewCodeDIO2-ViewCodeDIO2UITests/Pods-ViewCodeDIO2-ViewCodeDIO2UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViewCodeDIO2_ViewCodeDIO2UITests : NSObject
+@end
+@implementation PodsDummy_Pods_ViewCodeDIO2_ViewCodeDIO2UITests
+@end
---
+++ ViewCodeDIO2/Common/ViewCode.swift
@@ -0,0 +1,29 @@
+//
+//  ViewCode.swift
+//  MazeTv
+//
+//  Created by Karolina Attekita on 10/02/22.
+//
+
+import Foundation
+
+protocol ViewCode {
+    func buildHierarchy()
+    func setupConstraints()
+    func applyAdditionalChanges()
+}
+
+extension ViewCode {
+
+    func setupView() {
+        buildHierarchy()
+        setupConstraints()
+        applyAdditionalChanges()
+    }
+
+    func buildHierarchy() {}
+
+    func setupConstraints() {}
+
+    func applyAdditionalChanges() {}
+}
